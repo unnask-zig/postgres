1f616dd3ad89ead79d1a324d8d39a1a72a13d487
unnask <8060462+unnask@users.noreply.github.com>
2024-01-25 21:42:15 -0600

    little work. cleanup. comments

--- src/protocol/message/backend.zig
+++ src/protocol/message/backend.zig
@@ -1,31 +1,54 @@
 const std = @import("std");
 
-const AuthenticationMD5Password = struct {
+const AuthMD5Password = struct {
     salt: [4]u8 = undefined,
 };
 
+const BackendKeyData = struct {
+    process: i32,
+    secret: i32,
+};
+
 const BackendMessage = union(enum) {
-    authenticationOk,
-    authenticationCleartextPass,
-    //kerberos
-    authenticationMD5Pass: AuthenticationMD5Password,
+    authOk,
+    authCleartextPass,
+    //authKerberosV5,
+    authMD5Pass: AuthMD5Password,
+    //authSCMCred,
+    //authGSS,
+    //authGSSContinue,
     unsupported,
 };
 
-inline fn deserializeAuthentication(message: []const u8) BackendMessage {
+//authKerberosV5
+//authSCMCredential
+//authGSS
+//authGSSContinue
+//authSSPI
+//authSASL
+//authSASLContinue
+//authSASLFinal
+inline fn deserializeauth(message: []const u8) BackendMessage {
     // The spec details that this is actually an int32, however, the max
     // value is 12, so no need to do this extra work for the moment.
     //const msgType = std.mem.bigToNative(i32, std.mem.bytesAsValue(i32, message[5..9]).*);
     const msgType = message[8];
     switch (msgType) {
-        0 => return .authenticationOk,
+        0 => return .authOk,
         2 => return .unsupported, //kerberos
-        3 => return .authenticationCleartextPass,
+        3 => return .authCleartextPass,
         5 => {
-            var tmp = AuthenticationMD5Password{};
+            var tmp = AuthMD5Password{};
             @memcpy(&tmp.salt, message[9..13]);
-            return BackendMessage{ .authenticationMD5Pass = tmp };
+            return BackendMessage{ .authMD5Pass = tmp };
         },
+        6 => return .unsupported, //authSCMCredential
+        7 => return .unsupported, //authGSS
+        8 => return .unsupported, //authGSSContinue
+        9 => return .unsupported, //authSSPI
+        10 => return .unsupported, //authSASL
+        11 => return .unsupported, //authSASLContinue
+        12 => return .unsupported, //authSASLFinal
         else => return .unsupported,
     }
     return .unsupported;
@@ -46,22 +69,12 @@ pub fn deserialize(message: []const u8) !BackendMessage {
     }
 
     return switch (message[0]) {
-        'R' => deserializeAuthentication(message),
+        'R' => deserializeauth(message),
+        'K' => .unsupported,
         else => .unsupported,
     };
 }
 
-//authenticationOk
-//authenticationKerberosV5
-//authenticationCleartextPass
-//authenticationMD5Pass
-//authenticationSCMCredential
-//authenticationGSS
-//authenticationGSSContinue
-//authenticationSSPI
-//authenticationSASL
-//authenticationSASLContinue
-//authenticationSASLFinal
 //backendKeyData
 //bindComplete
 //closeComplete
@@ -86,28 +99,28 @@ pub fn deserialize(message: []const u8) !BackendMessage {
 //readyForQuery
 //rowDescription
 
-test "BackendMessage.authenticationOK good message" {
+test "BackendMessage.authOK good message" {
     const msg = [_]u8{ 'R', 0, 0, 0, 8, 0, 0, 0, 0 };
 
     const des = try deserialize(&msg);
 
-    try std.testing.expectEqual(des, BackendMessage.authenticationOk);
+    try std.testing.expectEqual(des, BackendMessage.authOk);
 }
 
-test "BackendMessage.authenticationCleartextPass good message" {
+test "BackendMessage.authCleartextPass good message" {
     const msg = [_]u8{ 'R', 0, 0, 0, 8, 0, 0, 0, 3 };
 
     const des = try deserialize(&msg);
 
-    try std.testing.expectEqual(des, BackendMessage.authenticationCleartextPass);
+    try std.testing.expectEqual(des, BackendMessage.authCleartextPass);
 }
 
-test "BackendMessage.authenticationMD5Pass good message" {
+test "BackendMessage.authMD5Pass good message" {
     const msg = [_]u8{ 'R', 0, 0, 0, 12, 0, 0, 0, 5, 1, 2, 3, 4 };
 
     const des = try deserialize(&msg);
-    var tmp = AuthenticationMD5Password{};
+    var tmp = AuthMD5Password{};
     tmp.salt = [4]u8{ 1, 2, 3, 4 };
 
-    try std.testing.expectEqual(des, BackendMessage{ .authenticationMD5Pass = tmp });
+    try std.testing.expectEqual(des, BackendMessage{ .authMD5Pass = tmp });
 }
