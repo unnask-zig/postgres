8f5a569e8fd1d7b3182c38446d93d40f46b539c9
unnask <8060462+unnask@users.noreply.github.com>
2024-04-19 21:47:04 -0600

    start frontend

--- src/protocol/message/frontend.zig
+++ src/protocol/message/frontend.zig
@@ -6,7 +6,7 @@
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const Writer = @import("message_buffer.zig").MessageWriter;
+const Writer = @import("message_buffer.zig").MessageBufferWriter;
 
 //bind(F) message.
 //in general, we're just going to tack everythings bytes together, but this
@@ -42,6 +42,54 @@ pub fn bind(writer: Writer, portal: u8, stmt: []u8, formats: []i16, values: []u8
 //saslInitialResponse
 //saslResponse
 //sslRequest
+
 //startupMessage
+pub fn startupMessage(writer: Writer, user: []const u8, database: ?[]const u8, options: ?[]const u8, replication: ?[]const u8) !void {
+    const version: i32 = 196608;
+
+    //I went with an ArrayList type before considering that I'd need to write
+    //to a specific spot in the list. Looks like ArrayList + its writer
+    //is not going to handle that all that well as it is not seekable.
+    //annoying!
+    try writer.writeInt(i32, 0, std.builtin.Endian.big);
+    try writer.writeInt(i32, version, std.builtin.Endian.big);
+    try writer.write("user");
+    try writer.writeByte(0);
+    try writer.write(user);
+    try writer.writeByte(0);
+    if (database) |db| {
+        try writer.write("user");
+        try writer.writeByte(0);
+        try writer.write(db);
+        try writer.writeByte(0);
+    }
+    if (options) |opts| {
+        try writer.write("options");
+        try writer.writeByte(0);
+        try writer.write(opts);
+        try writer.writeByte(0);
+    }
+    if (replication) |rep| {
+        try writer.write("repication");
+        try writer.writeByte(0);
+        try writer.write(rep);
+        try writer.writeByte(0);
+    }
+
+    try writer.writeByte(0);
+}
+
 //sync
 //terminate
+
+const MessageBuffer = @import("message_buffer.zig").MessageBuffer;
+test "startupMessage only user" {
+    const buf = MessageBuffer.init(std.testing.allocator);
+
+    const msg = startupMessage(buf.writer, "test", null, null, null, null);
+
+    const compare = []u8{ 0, 0, 0, 19, 0, 0, 3, 0, 'u', 's', 'e', 'r', 0, 't', 'e', 's', 't', 0, 0 };
+
+    _ = msg;
+    _ = compare;
+}
