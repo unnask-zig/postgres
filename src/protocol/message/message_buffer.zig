ea414c0d1200e9d0ef5797f746e64d3015ac851f
unnask <8060462+unnask@users.noreply.github.com>
2024-04-20 17:31:17 -0600

    comment

--- src/protocol/message/message_buffer.zig
+++ src/protocol/message/message_buffer.zig
@@ -1,4 +1,10 @@
 const std = @import("std");
 
+//Guess this isn't going to work so well due to needing length after writing.
+//I mean, technically, just calcualate the length ahead of time, then write,
+//but that seems annoying
 pub const MessageBuffer = std.ArrayList(u8);
 pub const MessageBufferWriter = MessageBuffer.Writer;
+
+// Going to just write a separate repo that implements the generic
+// reader and writer for a byte stream
