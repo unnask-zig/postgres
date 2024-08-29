524e0e59f22a2ca86e954d6d675714fb61340e9b
unnask <8060462+unnask@users.noreply.github.com>
2024-08-28 23:25:59 -0600

    write some of the frontend fns

--- src/protocol/message/frontend.zig
+++ src/protocol/message/frontend.zig
@@ -18,18 +18,16 @@ inline fn writeSize(buffer: *Buffer, pos: usize) void {
     //maybe ArrayList isn't the approach to take
     const size: i32 = @intCast(buffer.items.len);
 
-    std.mem.writeInt(i32, &buffer.items[pos..][0..4], size, std.builtin.Endian.big);
+    std.mem.writeInt(i32, &buffer.items[pos..][0..4], size - pos, std.builtin.Endian.big);
 }
 
-inline fn writeStr(writer: Buffer.Writer, str: []u8) !void {
+inline fn writeStr(writer: *Buffer.Writer, str: []u8) !void {
     try writer.writeAll(str);
     try writer.writeByte(0);
 }
 
 //bind(F) message.
-pub fn bind(buffer: Buffer, portal: u8, stmt: []u8, formats: []i16, values: [][]u8, result_formats: []i16) !void {
-    buffer.resize(0);
-
+pub fn bind(buffer: *Buffer, portal: [:0]const u8, stmt: [:0]const u8, formats: []const i16, values: []const []const u8, result_formats: []i16) !void {
     var writer = buffer.writer();
     try writer.writeByte('B');
     try writer.writeInt(i32, 0, std.builtin.Endian.big);
@@ -37,71 +35,160 @@ pub fn bind(buffer: Buffer, portal: u8, stmt: []u8, formats: []i16, values: [][]
     try writeStr(portal);
     try writeStr(stmt);
 
-    writer.writeInt(i16, @intCast(formats.len), std.builtin.Endian.big);
+    try writer.writeInt(i16, @intCast(formats.len), std.builtin.Endian.big);
     for (formats) |format| {
-        writer.writeInt(i16, format, std.builtin.Endian.big);
+        try writer.writeInt(i16, format, std.builtin.Endian.big);
     }
 
-    writer.writeInt(i16, @intCast(values.len), std.builtin.Endian.big);
+    try writer.writeInt(i16, @intCast(values.len), std.builtin.Endian.big);
     for (values) |value| {
-        writer.writeInt(i32, @intCast(value.len), std.builtin.Endian.big);
-        writer.writeAll(value);
+        try writer.writeInt(i32, @intCast(value.len), std.builtin.Endian.big);
+        try writer.writeAll(value);
     }
 
-    writer.writeInt(i16, @intCast(result_formats.len), std.builtin.Endian.big);
+    try writer.writeInt(i16, @intCast(result_formats.len), std.builtin.Endian.big);
     for (result_formats) |result_format| {
-        writer.writeInt(i16, result_format, std.builtin.Endian.big);
+        try writer.writeInt(i16, result_format, std.builtin.Endian.big);
     }
+
+    writeSize(buffer, 1);
 }
 
-//cancelRequest
-//close
+pub fn cancelRequest(buffer: *Buffer, process_id: i32, secret: i32) !void {
+    const cancel_code: i32 = 80877102;
+
+    var writer = buffer.writer();
+    try writer.writeInt(i32, 16, std.builtin.Endian.big);
+    try writer.writeInt(i32, cancel_code, std.builtin.Endian.big);
+    try writer.writeInt(i32, process_id, std.builtin.Endian.big);
+    try writer.writeInt(i32, secret, std.builtin.Endian.big);
+
+    writeSize(buffer, 0);
+}
+
+const StatementType = enum {
+    portal,
+    prepared_statement,
+};
+pub fn close(buffer: *Buffer, comptime close_type: StatementType, name: [:0]const u8) !void {
+    var writer = buffer.writer();
+
+    try writer.writeByte('C');
+    try writer.writeInt(i32, 0, std.builtin.Endian.big);
+    switch (close_type) {
+        StatementType.portal => try writer.writeByte('P'),
+        StatementType.prepared_statement => try writer.writeByte('S'),
+    }
+    try writer.writeAll(name);
+    writeSize(buffer, 1);
+}
+
 //copyData
+pub fn copyData(buffer: *Buffer, bytes: []const u8) !void {
+    var writer = buffer.writer();
+
+    //todo: naive implementation. may needs fixing. might make more sense to
+    //      fix in whatever calls this function
+    const size: i32 = @intCast(bytes.len + 4);
+
+    try writer.writeByte('d');
+    try writer.writeInt(i32, size, std.builtin.Endian.big);
+    try writer.writeAll(bytes);
+}
+
 //copyDone
+pub fn copyDone(buffer: *Buffer) !void {
+    var writer = buffer.writer();
+    try writer.writeByte('c');
+    try writer.writeByte(4);
+}
+
 //copyFail
+pub fn copyFail(buffer: *Buffer, msg: [:0]const u8) !void {
+    var writer = buffer.writer();
+    try writer.writeByte('f');
+    try writer.writeInt(i32, 0, std.builtin.Endian.big);
+    try writer.writeAll(msg);
+    writeSize(buffer, 1);
+}
+
 //describe
+pub fn describe(buffer: *Buffer, comptime describe_type: StatementType, name: [:0]const u8) !void {
+    var writer = buffer.writer();
+    try writer.writeByte('D');
+    try writer.writeInt(i32, 0, std.builtin.Endian.big);
+    switch (describe_type) {
+        StatementType.portal => try writer.writeByte('P'),
+        StatementType.prepared_statement => try writer.writeByte('S'),
+    }
+    try writer.writeAll(name);
+    writeSize(buffer, 1);
+}
+
 //execute
+pub fn execute(buffer: *Buffer, portal: [:0]const u8, max_rows: i32) !void {
+    var writer = buffer.writer();
+
+    try writer.writeByte('E');
+    try writer.writeInt(i32, 0, std.builtin.Endian.big);
+    try writer.writeAll(portal);
+    try writer.writeInt(i32, max_rows, std.builtin.Endian.big);
+    writeSize(buffer, 1);
+}
+
 //flush
+pub fn flush(buffer: *Buffer) !void {
+    var writer = buffer.writer();
+
+    writer.writeByte('H');
+    writer.writeInt(i32, 4, std.builtin.Endian.big);
+}
+
 //functionCall
+
 //gssencRequest
+
 //gssResponse
+
 //parse
+
 //passwordMessage
+
 //query
+
 //saslInitialResponse
+
 //saslResponse
+
 //sslRequest
 
 //startupMessage
-pub fn startupMessage(buffer: Buffer, user: []const u8, database: ?[]const u8, options: ?[]const u8, replication: ?[]const u8) !void {
+pub fn startupMessage(buffer: *Buffer, user: [:0]const u8, database: ?[:0]const u8, options: ?[:0]const u8, replication: ?[:0]const u8) !void {
     const version: i32 = 196608;
 
     buffer.resize(0);
     var writer = buffer.writer();
 
+    const user_text: [:0]const u8 = "user";
+    const database_text: [:0]const u8 = "database";
+    const options_text: [:0]const u8 = "options";
+    const replication_text: [:0]const u8 = "replication";
+
     try writer.writeInt(i32, 0, std.builtin.Endian.big);
     try writer.writeInt(i32, version, std.builtin.Endian.big);
-    try writer.writeAll("user");
-    try writer.writeByte(0);
+    try writer.writeAll(user_text);
     try writer.writeAll(user);
-    try writer.writeByte(0);
     if (database) |db| {
-        try writer.writeAll("user");
-        try writer.writeByte(0);
+        try writer.writeAll(database_text);
         try writer.writeAll(db);
-        try writer.writeByte(0);
     }
     if (options) |opts| {
-        try writer.writeAll("options");
-        try writer.writeByte(0);
+        try writer.writeAll(options_text);
         try writer.writeAll(opts);
-        try writer.writeByte(0);
     }
     if (replication) |rep| {
-        try writer.writeAll("repication");
-        try writer.writeByte(0);
+        try writer.writeAll(replication_text);
         try writer.writeAll(rep);
-        try writer.writeByte(0);
     }
 
     try writer.writeByte(0);
